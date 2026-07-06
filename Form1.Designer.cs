99902c26c9fe1afacb0ab40aee2dd0e9109044f7
mainard83 <mainard83@gmail.com>
2026-07-06 20:12:02 +0200

    Add designer code for MainForm initialization

---
+++ Form1.Designer.cs
@@ -0,0 +1,22 @@
+namespace yWrite
+{
+    partial class MainForm
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+        }
+    }
+}
