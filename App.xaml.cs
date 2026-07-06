13e024db5f9daecc24541bb0c7bb124c1cc3a980
mainard83 <mainard83@gmail.com>
2026-07-06 19:47:56 +0200

    Refactor App.xaml.cs for UWP compatibility

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,22 +1,45 @@
-using Microsoft.UI.Xaml;
-using Microsoft.UI.Xaml.Controls;
+using Windows.ApplicationModel;
+using Windows.ApplicationModel.Activation;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Navigation;
 
 namespace yWrite_UWP
 {
-    public partial class App : Application
+    sealed partial class App : Application
     {
         public App()
         {
-            this.InitializeComponent();
+            InitializeComponent();
+            Suspending += OnSuspending;
         }
 
-        protected override void OnLaunched(Microsoft.UI.Xaml.LaunchActivatedEventArgs args)
+        protected override void OnLaunched(LaunchActivatedEventArgs e)
         {
-            var window = new Window();
-            window.Content = new MainPage();
-            window.Title = "yWrite";
-            window.AppWindow.SetIcon("Assets/StoreLogo.png");
-            window.Activate();
+            Frame rootFrame = Window.Current.Content as Frame;
+            if (rootFrame == null)
+            {
+                rootFrame = new Frame();
+                rootFrame.NavigationFailed += OnNavigationFailed;
+                Window.Current.Content = rootFrame;
+            }
+
+            if (e.PrelaunchActivated == false)
+            {
+                if (rootFrame.Content == null)
+                {
+                    rootFrame.Navigate(typeof(MainPage), e.Arguments);
+                }
+                Window.Current.Activate();
+            }
+        }
+
+        void OnNavigationFailed(object sender, NavigationFailedEventArgs e) => throw new Exception("Failed to load Page");
+
+        void OnSuspending(object sender, SuspendingEventArgs e)
+        {
+            var deferral = e.SuspendingOperation.GetDeferral();
+            // Save state if needed
+            deferral.Complete();
         }
     }
 }
